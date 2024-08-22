d526908e1f8bdd6dd2bd5a0d785f61402ba325dd
daddychlt <134816209+daddychlt@users.noreply.github.com>
2024-08-22 10:52:26 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+KV = '''
+BoxLayout:
+    orientation: 'vertical'
+    MDTopAppBar:
+        title: 'My KivyMD App'
+        elevation: 10
+    MDLabel:
+        text: 'Hello, KivyMD!'
+        halign: 'center'
+'''
+
+class MyApp(MDApp):
+    def build(self):
+        return Builder.load_string(KV)
+
+if __name__ == '__main__':
+    MyApp().run()
